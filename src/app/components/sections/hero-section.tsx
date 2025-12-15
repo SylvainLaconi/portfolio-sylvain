a6479a9497b400d18cd406729183fd804b182080
Sylvain-xstudio <sylvain.laconi@x-studio.fr>
2025-12-15 09:09:08 +0100

    hero section layout

--- src/app/components/sections/hero-section.tsx
+++ src/app/components/sections/hero-section.tsx
@@ -8,19 +8,25 @@ export default function HeroSection() {
       <div className="w-full bg-[url('/images/hero-image.webp')] bg-contain bg-right bg-no-repeat">
         <div className="relative container mx-2.5 flex flex-col gap-4 py-24 md:mx-auto md:gap-8 md:py-48">
           <AnimateOnScroll delay={200}>
-            <span className="text-2xl font-bold text-black md:text-4xl">
-              Freelance fullstack
-            </span>
+            <div className="flex min-h-8 items-center">
+              <span className="min-h-8 text-lg font-bold text-black md:text-4xl">
+                Freelance fullstack
+              </span>
+            </div>
           </AnimateOnScroll>
 
           <AnimateOnScroll delay={400}>
-            <h1 className="text-4xl font-bold md:text-7xl">Sylvain Laconi</h1>
+            <h1 className="min-h-8 text-2xl font-bold md:text-7xl">
+              Sylvain Laconi
+            </h1>
           </AnimateOnScroll>
 
           <AnimateOnScroll delay={600}>
-            <h2 className="text-sm font-extrabold uppercase md:text-2xl">
-              React.js, Next.js, Webflow & Retool
-            </h2>
+            <div className="flex min-h-8 items-center">
+              <h2 className="text-xs font-extrabold uppercase md:text-2xl">
+                React.js, Next.js, Webflow & Retool
+              </h2>
+            </div>
           </AnimateOnScroll>
 
           <AnimateOnScroll delay={800}>
